59dfbdb0e8a625e005700d694b9f262ffb0e396e
KarolG96 <karol.g.96@gmail.com>
2023-02-15 00:08:17 +0100

    Upgrade1

---
+++ Book_store/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Book_store.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Book_store.Data
+{
+	public class ApplicationDbContext :DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+
+
+		public DbSet<Category> Categories { get; set; }
+	}
+}
+
---
+++ Book_store/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Book_store.Models
+{
+	public class Category
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string Name { get; set; }
+		public int DisplayOrder { get; set; }
+		public DateTime CreatedDataTime { get; set; } = DateTime.Now;
+
+	}
+}
+
