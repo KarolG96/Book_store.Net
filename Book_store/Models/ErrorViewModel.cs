9589ca12610cbb4cb116161ce578f2a3c127f3f0
KarolG96 <karol.g.96@gmail.com>
2023-02-14 23:09:29 +0100

    BookApp

---
+++ Book_store/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Book_store.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
