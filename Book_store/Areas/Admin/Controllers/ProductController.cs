6fdccd3009741f37c3e150794c22c6d9774b6bf6
KarolG <karol.g.96@icloud.com>
2023-02-17 21:04:18 +0100

    Update.1.9.EndOfProd

--- Book_store/Areas/Admin/Controllers/ProductController.cs
+++ Book_store/Areas/Admin/Controllers/ProductController.cs
@@ -108,42 +108,7 @@ namespace BookStore.Areas.Admin.Controllers
             }
             return View(obj);
         }
-
-        //GET
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            //var categoryFromDb = _db.Categories.Find(id);
-            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
-
-            if (CoverTypeFromDbFirst == null)
-            {
-                return NotFound();
-            }
-
-            return View(CoverTypeFromDbFirst);
-        }
-
-        //POST
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int? id)
-        {
-            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.CoverType.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "CoverType deleted successfully";
-            return RedirectToAction("Index");
-
-        }
+        
 
         #region API CALLS
         [HttpGet]
@@ -152,6 +117,28 @@ namespace BookStore.Areas.Admin.Controllers
             var productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
             return Json(new { data=productList });
         }
+
+		//POST
+		[HttpDelete]
+		public IActionResult Delete(int? id)
+		{
+			var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+			if (obj == null)
+			{
+                return Json(new { success = false, massage = "Error while delting" });
+			}
+
+			var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+			if (System.IO.File.Exists(oldImagePath))
+			{
+				System.IO.File.Delete(oldImagePath);
+			}
+
+			_unitOfWork.Product.Remove(obj);
+			_unitOfWork.Save();
+			return Json(new { success = true, massage = "Delete successful" });
+
+		}
 
 		#endregion
 	}
