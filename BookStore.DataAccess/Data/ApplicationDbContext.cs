97ea59226e7533a51eb82ff94aa768bee673c5e0
KarolG <karol.g.96@icloud.com>
2023-02-17 11:50:20 +0100

    Update.1.7.Product

--- BookStore.DataAccess/Data/ApplicationDbContext.cs
+++ BookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace BookStore.DataAcess
 
 		public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
 
---
+++ BookStore.Models/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Range(1,10000)]
+        public double ListPrice { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+
+        public string ImageURL { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+        [Required]
+        public int CoverTypeId { get; set; }
+
+        public CoverType CoverType { get; set; }
+    }
+}
+
