291d5dc05a7c492a8f02402774b99fdccd7bbd34
KarolG <karol.g.96@icloud.com>
2023-02-18 04:46:37 +0100

    Update.2.0.1

---
+++ BookStore.Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookStore.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		[Required]
+		public string Name { get; set; }
+		public string? Address { get; set; }
+		public string? City { get; set; }
+		public string? Region { get; set; }
+		public string? PostalCode { get; set; }
+
+	}
+}
