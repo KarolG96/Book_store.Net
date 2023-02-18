35344193279cf088f8c43954b7392207009130a8
KarolG96 <karol.g.96@icloud.com>
2023-02-18 22:46:27 +0100

    Test zmian

--- BookStore.Models/ApplicationUser.cs
+++ BookStore.Models/ApplicationUser.cs
@@ -12,9 +12,9 @@ namespace BookStore.Models
 	{
 		[Required]
 		public string Name { get; set; }
-		public string? Address { get; set; }
+		public string? StreetAddress { get; set; }
 		public string? City { get; set; }
-		public string? Region { get; set; }
+		public string? State { get; set; }
 		public string? PostalCode { get; set; }
 
 	}
